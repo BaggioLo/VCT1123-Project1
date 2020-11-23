3280e0f0aff8c8895ce513338efcacc17b25cbae
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2020-11-22 19:19:39 -0600

    Created Colors array and assigned them color variables

--- js/app.js
+++ js/app.js
@@ -25,3 +25,5 @@ var color1 = "linear-gradient(135deg, #0B132B 0%, #3A506B 100%)";
 var color2 = "linear-gradient(330deg, #5BC0BE 0%, #3A506B 100%)";
 var color3 = "linear-gradient(35deg, #0B132B 0%, #5BC0BE 100%)";
 var color4 = "linear-gradient(235deg, #5BC0BE 0%, #3A506B 100%)";
+
+colorsArray = [color1, color2, color3, color4];
