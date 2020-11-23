72f1b481f4c5c16d02c19f9a4494b90c693f3ae8
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2020-11-22 19:14:45 -0600

    Create variable wrapper, added class wrapper. Created 4 divs with for loop within wrapper. has class wrapperDivs

--- js/app.js
+++ js/app.js
@@ -5,4 +5,19 @@ addToHead.appendChild(cssLink);
 cssLink.rel = "stylesheet";
 cssLink.href = "css/style.css";
 
+// Create a variable wrapper //
+
+var bodY = document.querySelector('body');
+var wrapper = document.createElement('div');
+bodY.appendChild(wrapper);
+
+document.getElementsByTagName('div')[0].classList.add('wrapper');
+
+// div is properly added into the body //
+
+for (var i = 0; i < 4; i++) {
+    var divs = document.createElement("div");
+    divs.classList = 'wrapperDivs';
+    wrapper.appendChild(divs);
+}
 
