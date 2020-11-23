e7837c276406139625bd0fdffaab415a2e636c25
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2020-11-22 19:18:29 -0600

    Created 4 color variables and assigned them linear gradient

--- js/app.js
+++ js/app.js
@@ -21,3 +21,7 @@ for (var i = 0; i < 4; i++) {
     wrapper.appendChild(divs);
 }
 
+var color1 = "linear-gradient(135deg, #0B132B 0%, #3A506B 100%)";
+var color2 = "linear-gradient(330deg, #5BC0BE 0%, #3A506B 100%)";
+var color3 = "linear-gradient(35deg, #0B132B 0%, #5BC0BE 100%)";
+var color4 = "linear-gradient(235deg, #5BC0BE 0%, #3A506B 100%)";
